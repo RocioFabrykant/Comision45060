b087c9861da859663aaab2b8e8b51946757fc14c
Rocio Fabrykant <fabrykantr@gmail.com>
2022-11-27 16:59:16 -0300

    ultimo commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,22 +5,22 @@ import {Link} from 'react-router-dom';
 
 const Cart = () => {
   const {cart,deleteOne,deleteAll,precioTotal} = useContext(CartContext);
-  if(cart.length === 0) return( <h1>El carrito está vacío <Link to="/">Home</Link></h1>);
+  if(cart.length === 0) return( <h1>El carrito está vacío <Link to="/">Ir al HOME</Link></h1>);
   return(
-    <div>
+    <div className='carrito'>
       {cart.map((prod) => (
         <div>
           <img src={prod.img} alt={prod.title} width="80px" />
           <div>
             <h3>{prod.title}</h3>
-            <h3>{prod.cantidad}</h3>
+            <h3>Cantidad {prod.cantidad}</h3>
             <button onClick={()=> deleteOne(prod.id)}>Eliminar</button>
           </div>
         </div>
       ))}
       <h2>Total:${precioTotal()}</h2>
       <button onClick={deleteAll}>Vaciar carrito</button>
-      <br /><Link to="/checkout">Comprar</Link>
+      <br /><br /><Link to="/checkout">Checkout</Link>
     </div>
   )
       
